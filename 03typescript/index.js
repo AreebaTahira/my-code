dc4011c8594e127b5984281a16ea6f87e9068727
AreebaTahira <76103465+AreebaTahira@users.noreply.github.com>
2023-10-17 21:07:15 +0500

    Add files via upload

---
+++ 03typescript/index.js
@@ -0,0 +1,18 @@
+// let specialValue = null;
+// console.log(specialValue);
+// let firstName = "areeba";
+// let lastName = "tahira";
+// let personName = "areeba tahira";
+// // back tick
+// console.log(`hello ${personName}i am from panaverse`);
+// // typeof
+// console.log(typeof personName);
+// template litrtals//
+// console.log(3 +"3");
+// console.log("typeof" , typeof (3+"3"));
+// export{};
+var myfirstName = "areeba";
+console.log(myfirstName);
+var mylastName = "tahira";
+console.log("hello i am from pattoki ".concat(myfirstName));
+console.log(typeof myfirstName);
---
+++ 03typescript/index.ts
@@ -0,0 +1,51 @@
+
+// let specialValue = null;
+// console.log(specialValue);
+
+// let firstName = "areeba";
+// let lastName = "tahira";
+
+// let personName = "areeba tahira";
+
+// // back tick
+
+// console.log(`hello ${personName}i am from panaverse`);
+
+// // typeof
+
+// console.log(typeof personName);
+
+
+// template litrtals//
+
+// console.log(3 +"3");
+// console.log("typeof" , typeof (3+"3"));
+
+
+
+// export{};
+
+
+
+
+
+
+
+
+
+
+
+
+let myfirstName : string = "areeba";
+console.log(myfirstName);
+
+
+let mylastName : string = "tahira";
+console.log(`hello i am from pattoki ${myfirstName}`);
+
+console.log(typeof myfirstName);
+
+
+
+
+
